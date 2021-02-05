869419e5c2daaa3c9d09c2c432b82a8c470ef884
michellebakels <michelle.bakels@gmail.com>
2021-02-05 14:09:16 -0500

    class to functional component

--- src/WildHabEventForm.jsx
+++ src/WildHabEventForm.jsx
@@ -1,29 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-class WildHabEventForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            eventName: '',
-            sport: '',
-            eventDuration: 0
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.submitForm = this.submitForm.bind(this)
-    }
+const WildHabEventForm = () => {
+    const [formValues, setFormValues] = useState({
+        eventName: '',
+        sport: '',
+        eventDuration: 0
+    })
 
-    handleChange(event) {
-        this.setState({[event.target.name]: event.target.value})
-    }
-
-    submitForm(event) {
-        console.log(this.state)
+    const submitForm = (event) => {
+        console.log(formValues)
         fetch("https://wildhab-api-a.web.app/events", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(this.state),
+            body: JSON.stringify(formValues),
         })
             .then(result => result.json())
             .then(data => console.log(JSON.stringify(data)))
@@ -31,46 +22,45 @@ class WildHabEventForm extends React.Component {
         event.preventDefault()
     }
 
-    render() {
-        return(
-            <>
-                <h1>Create Wild Habitat Event</h1>
-                <form onSubmit={this.submitForm}>
-                    <label>
-                        Event Name:&nbsp;
-                    </label>
-                    <input
-                        name="eventName"
-                        type="text"
-                        value={this.state.eventName}
-                        onChange={this.handleChange}
-                    />
-                    <br />
-                    <label>
-                        Sport:&nbsp;
-                    </label>
-                    <input
-                        name="sport"
-                        type="text"
-                        value={this.state.sport}
-                        onChange={this.handleChange}
-                    />
-                    <br />
-                    <label>
-                        Event Duration (hours):&nbsp;
-                    </label>
-                    <input
-                        name="eventDuration"
-                        type="number"
-                        value={this.state.eventDuration}
-                        onChange={this.handleChange}
-                    />
-                    <br />
-                    <button type="submit">Submit</button>
-                </form>
-            </>
-        )
-    }
+    return(
+        <>
+            <h1>Create Wild Habitat Event</h1>
+            <form onSubmit={(event) => submitForm(event)}>
+                <label>
+                    Event Name:&nbsp;
+                </label>
+                <input
+                    name="eventName"
+                    type="text"
+                    value={formValues.eventName}
+                    onChange={e => setFormValues({...formValues, eventName: e.target.value})}
+                />
+                <br />
+                <label>
+                    Sport:&nbsp;
+                </label>
+                <input
+                    name="sport"
+                    type="text"
+                    value={formValues.sport}
+                    onChange={e => setFormValues({...formValues, sport: e.target.value})}
+                />
+                <br />
+                <label>
+                    Event Duration (hours):&nbsp;
+                </label>
+                <input
+                    name="eventDuration"
+                    type="number"
+                    value={formValues.eventDuration}
+                    onChange={e => setFormValues({...formValues, eventDuration: e.target.value})}
+                />
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
+
 }
 
 export default WildHabEventForm
