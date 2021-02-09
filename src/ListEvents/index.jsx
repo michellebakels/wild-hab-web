a3f31a2665e3277c9f6af232b879a08084ab3a74
Todd Albert <todd@bocacode.com>
2021-02-09 12:10:58 -0500

    went through finishing this with class

--- src/ListEvents/index.jsx
+++ src/ListEvents/index.jsx
@@ -1,19 +1,22 @@
-import React, {useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 const ListEvents = () => {
-
+    const [eventsList, setEventsList] = useState([])
     useEffect(() => {
         fetch("https://wildhab-api-a.web.app/events")
             .then(result => result.json())
-            .then(data => console.log(data))
+            .then(data => setEventsList(data.data))
             .catch(error => console.log('error', error))
     }, [])
-
-    // store events in state
-    // display events
-
+    // store events in state √
+    // display events √
     return(
-        <div>Hello List Events</div>
+        <>
+            <div>Hello List Events</div>
+            {eventsList && eventsList.map(event =>
+                <p>{event.eventName || event.name}, {event.sport}, Duration: {event.eventDuration}</p>
+            )}
+        </>
     )
 }
 
